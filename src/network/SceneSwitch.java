c249e34ed6be6047a3ad9fc444088ae1fb1e4acd
s2011026 <g.n.wemekamp@student.utwente.nl>
2018-04-11 11:08:33 +0000

    GUI updated

--- src/network/SceneSwitch.java
+++ src/network/SceneSwitch.java
@@ -1,94 +1,191 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import java.util.ArrayList;
 
 
 public class SceneSwitch extends Application {
     private static final int HEIGHT = 680;
     private static final int WIDTH = 400;
-    private static final int PADDING = 35;
-
+    private static final int PADDING = 10;
+    private static final int INPUT_CHAT_HEIGHT = 50;
+    private static final int NUM_OF_CHATS = 5;
     private Stage window;
+
+    private ArrayList<String> messagesFirst, messagesSecond, messagesThird, messagesFourth, messagesGlobal;
+
+    private Button[] goToButtons = new Button[NUM_OF_CHATS];
+    private Button[] sendButtons = new Button[NUM_OF_CHATS];
+    private TextArea[] chatAreas = new TextArea[NUM_OF_CHATS];
+    private TextArea[] chatDisplays = new TextArea[NUM_OF_CHATS];
+
     private Scene firstChatScene, secondChatScene, thirdChatScene, fourthChatScene, globalChatScene;
-    private Button firstChatBtn, secondChatBtn, thirdChatBtn, fourthChatBtn, globalChatBtn;
-    private Label firstChatLabel, secondChatLabel, thirdChatLabel, fourthChatLabel, globalChatLabel;
-    private TextArea generalTextArea;
+    private Label[] chatLabels = new Label[NUM_OF_CHATS];
+    private Label[] chatLogLabels = new Label[NUM_OF_CHATS];
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        initializeButtons();
         initializeLabels();
+        initializeChatAreas();
+        initializeMessages();
+        initializeChatDisplay();
+        initializeButtons();
         window = primaryStage;
-        generalTextArea = new TextArea();
 
+        // First chat window
         VBox firstChatLayout = new VBox(PADDING);
-        System.out.println(thirdChatBtn.toString() + " " + fourthChatBtn.toString());
-        firstChatLayout.getChildren().addAll(firstChatLabel, secondChatBtn, thirdChatBtn,
-                fourthChatBtn, globalChatBtn, generalTextArea);
+        firstChatLayout.getChildren().addAll(chatLabels[0], goToButtons[1], goToButtons[2],
+                goToButtons[3], goToButtons[4], new Label("Send a message"),  chatAreas[0], sendButtons[0], chatLogLabels[0], chatDisplays[0]);
         firstChatScene = new Scene(firstChatLayout, WIDTH, HEIGHT);
+
+        // Refresh labels and buttons
         initializeButtons();
         initializeLabels();
 
-        VBox secondChatLayout = new VBox(34);
-        secondChatLayout.getChildren().addAll(secondChatLabel, firstChatBtn, thirdChatBtn,
-                fourthChatBtn, globalChatBtn, generalTextArea);
+        // Second chat window
+        VBox secondChatLayout = new VBox(PADDING);
+        secondChatLayout.getChildren().addAll(chatLabels[1], goToButtons[0], goToButtons[2],
+                goToButtons[3], goToButtons[4], new Label("Send a message"), chatAreas[1], sendButtons[1], chatLogLabels[1], chatDisplays[1]);
         secondChatScene = new Scene(secondChatLayout, WIDTH, HEIGHT);
+
+        // Refresh labels and buttons
         initializeButtons();
         initializeLabels();
 
-        VBox thirdChatLayout = new VBox(33);
-        thirdChatLayout.getChildren().addAll(thirdChatLabel, firstChatBtn, secondChatBtn,
-                fourthChatBtn, globalChatBtn);
+        // Third chat window
+        VBox thirdChatLayout = new VBox(PADDING);
+        thirdChatLayout.getChildren().addAll(chatLabels[2], goToButtons[0], goToButtons[1],
+                goToButtons[3], goToButtons[4], new Label("Send a message"), chatAreas[2], sendButtons[2], chatLogLabels[2], chatDisplays[2]);
         thirdChatScene = new Scene(thirdChatLayout, WIDTH, HEIGHT);
+
+        // Refresh labels and buttons
         initializeButtons();
         initializeLabels();
 
-        VBox fourthChatLayout = new VBox(32);
-        fourthChatLayout.getChildren().addAll(fourthChatLabel, firstChatBtn, secondChatBtn,
-                thirdChatBtn, globalChatBtn);
+        // Fourth chat window
+        VBox fourthChatLayout = new VBox(PADDING);
+        fourthChatLayout.getChildren().addAll(chatLabels[3], goToButtons[0], goToButtons[1],
+                goToButtons[2], goToButtons[4], new Label("Send a message"), chatAreas[3], sendButtons[3], chatLogLabels[3], chatDisplays[3]);
         fourthChatScene = new Scene(fourthChatLayout, WIDTH, HEIGHT);
+
+        // Refresh labels and buttons
         initializeButtons();
         initializeLabels();
 
-        VBox globalChatLayout = new VBox(31);
-        globalChatLayout.getChildren().addAll(globalChatLabel, firstChatBtn, secondChatBtn,
-                thirdChatBtn, fourthChatBtn);
+        // Global chat window
+        VBox globalChatLayout = new VBox(PADDING);
+        globalChatLayout.getChildren().addAll(chatLabels[4], goToButtons[0], goToButtons[1],
+                goToButtons[2], goToButtons[3], new Label("Send a message"), chatAreas[4], sendButtons[4], chatLogLabels[4], chatDisplays[4]);
         globalChatScene = new Scene(globalChatLayout, WIDTH, HEIGHT);
+
+        // Refresh labels and buttons
         initializeButtons();
         initializeLabels();
 
-        window.setScene(globalChatScene);
+        // Set the beginning scene
+        window.setScene(firstChatScene);
         window.setTitle("Best chatting application");
         window.show();
     }
 
+    /*
+        Initializes the Chat Displays (which display the chat log).
+     */
+    private void initializeChatDisplay() {
+        for (int i = 0; i < chatDisplays.length; i++) {
+            chatDisplays[i] = new TextArea();
+        }
+    }
+
+    /*
+        Initializes ArrayList<Strings> for keeping track of the messages that have been sent.
+    */
+    private void initializeMessages() {
+        messagesFirst = new ArrayList<>();
+        messagesFirst.add("test");
+        messagesFirst.add("this is a nice conversation");
+        messagesSecond = new ArrayList<>();
+        messagesThird = new ArrayList<>();
+        messagesFourth = new ArrayList<>();
+        messagesGlobal = new ArrayList<>();
+    }
+
+    /*
+        Initializes the input field for the chat ares.
+     */
+    private void initializeChatAreas() {
+        for (int i = 0; i < chatAreas.length; i++) {
+            chatAreas[i] = new TextArea();
+            chatAreas[i].setPrefHeight(INPUT_CHAT_HEIGHT);
+            chatAreas[i].setText("");
+        }
+    }
+
+    /*
+        Initializes top label which indicates with whom you are chatting. And the label that indicates the chat log.
+    */
     private void initializeLabels() {
-        firstChatLabel = new Label("You are chatting with person 1");
-        secondChatLabel = new Label("You are chatting with person 2");
-        thirdChatLabel = new Label("You are chatting with person 3");
-        fourthChatLabel = new Label("You are chatting with person 4");
-        globalChatLabel = new Label("You are chatting with every person that is reachable");
+        for (int i = 0; i < chatLabels.length; i++) {
+            chatLabels[i] = new Label("You are chatting with person " + (i + 1));
+            chatLogLabels[i] = new Label("Chat log with person " + (i + 1));
+        }
+        chatLabels[4].setText("Global Chat");
+        chatLabels[4].setText("Chat log on Global Chat");
     }
 
+    /*
+        Initializes the buttons for going to another scene and the sending button.
+    */
     private void initializeButtons() {
-        firstChatBtn = new Button("Go to chat 1");
-        secondChatBtn = new Button("Go to chat 2");
-        thirdChatBtn = new Button("Go to chat 3");
-        fourthChatBtn = new Button("Go to chat 4");
-        globalChatBtn = new Button("Go to global chat");
+        for (int i = 0; i < goToButtons.length; i++) {
+            goToButtons[i] = new Button("Go to chat " + (i + 1));
 
-        firstChatBtn.setOnAction(event -> window.setScene(firstChatScene));
-        secondChatBtn.setOnAction(event -> window.setScene(secondChatScene));
-        thirdChatBtn.setOnAction(event -> window.setScene(thirdChatScene));
-        fourthChatBtn.setOnAction(event -> window.setScene(fourthChatScene));
-        globalChatBtn.setOnAction(event -> window.setScene(globalChatScene));
+            Button sendTemp = new Button("Send");
+            sendTemp.setOnAction(buttonHandler);
+            sendButtons[i] = sendTemp;
+        }
+
+        goToButtons[0].setOnAction(event -> window.setScene(firstChatScene));
+        goToButtons[1].setOnAction(event -> window.setScene(secondChatScene));
+        goToButtons[2].setOnAction(event -> window.setScene(thirdChatScene));
+        goToButtons[3].setOnAction(event -> window.setScene(fourthChatScene));
+        goToButtons[4].setOnAction(event -> window.setScene(globalChatScene));
+        goToButtons[4].setText("Go to global chat");
     }
 
+    /*
+        This function gets called when the send button is clicked. The text that the user typed in an
+        TextArea is used here.
+     */
+    private EventHandler<ActionEvent> buttonHandler = event -> {
+        for (int i = 0; i < chatAreas.length; i++) {
+            System.out.println(event.getSource());
+
+            System.out.println(chatAreas[i].getText());
+
+
+            chatAreas[i].setText("");
+        }
+        event.consume();
+    };
+
+    /*
+        Run the program.
+     */
     public static void main(String[] args) {
         launch(args);
+    }
+
+    /*
+        Fills the chat logg with the messages that were sent
+     */
+    public void fillChatDisplays(ArrayList<String> messages, int chat) {
+
     }
 }
