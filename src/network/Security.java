0f9bef024ff3531bfe4d6d7c9abb79d232a543aa
Sorin Halmajan <sorinhal@yahoo.com>
2018-04-11 12:17:44 +0200

    security part

--- src/network/Security.java
+++ src/network/Security.java
@@ -1,12 +1,47 @@
 package network;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 public class Security {
 
-    public String decode(char destinatio, String message){
-        return "";
-    }
+   
+/**public static String encrypt(String strClearText,String strKey) throws Exception{
+	String strData="";
+	
+	try {
+		SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
+		Cipher cipher=Cipher.getInstance("Blowfish");
+		cipher.init(Cipher.ENCRYPT_MODE, skeyspec);
+		byte[] encrypted=cipher.doFinal(strClearText.getBytes());
+		strData=new String(encrypted);
+		
+	} catch (Exception e) {
+		e.printStackTrace();
+		throw new Exception(e);
+	}
+	return strData;
+	}
 
-    public String encode(char destinatio, String message){
-        return "";
-    }
+
+public static String decrypt(String strEncrypted,String strKey) throws Exception{
+	String strData="";
+	
+	try {
+		SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
+		Cipher cipher=Cipher.getInstance("Blowfish");
+		cipher.init(Cipher.DECRYPT_MODE, skeyspec);
+		byte[] decrypted=cipher.doFinal(strEncrypted.getBytes());
+		strData=new String(decrypted);
+		
+	} catch (Exception e) {
+		e.printStackTrace();
+		throw new Exception(e);
+	}
+	return strData;
+	}
+
+}**/
+	
+
 }
